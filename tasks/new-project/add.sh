3d93d0be2ed2edd5a3b41ca24f28b7d37093976b
gabrielkotaniZUP <gabriel.kotani@zup.com.br>
2022-10-09 14:22:21 -0300

    init

---
+++ tasks/new-project/add.sh
@@ -0,0 +1,2 @@
+echo "Adding project to workspace"
+vim :%s/"|--:|:-:|:-:|---|:-:|---|"/"|--:|:-:|:-:|---|:-:|---|\n|**{{name_project}}**|`Usar`|**experimentos**|Demonstração de uso de pastas| | 2022-10-09|"/g
